c229cc1dc5eda96630752c13507c748320974c0e
Dmitry Lorents <zmey.task@gmail.com>
2024-03-07 15:56:39 +0300

    Add log variants

--- Recepies/Services/Logging/LogCommand.swift
+++ Recepies/Services/Logging/LogCommand.swift
@@ -5,15 +5,25 @@ import Foundation
 
 /// Describes types of possible log commands
 enum LogCommand {
-    case one
-    case two
+    /// User did open screen with name (string value)
+    case userOpenScreen(screenName: String)
+    /// User did move  from category with name (first string value) to screen with name (second string value)
+    case userMove(categoryName: String, screenName: String)
+    /// User did open recipe with name (string value)
+    case openRecipe(recipeName: String)
+    /// User did share recipe with name (string value)
+    case shareRecipe(recipeName: String)
 
     var message: String {
         switch self {
-        case .one:
-            return "One"
-        case .two:
-            return "Two"
+        case .userOpenScreen(let screenName):
+            return "User open \(screenName) screen"
+        case .userMove(let categoryName, let screenName):
+            return "User moved from \(categoryName) category to \(screenName) screen"
+        case .openRecipe(let recipeName):
+            return "User open \(recipeName) recipe"
+        case .shareRecipe(let recipeName):
+            return "User share \(recipeName) recipe"
         }
     }
 }
